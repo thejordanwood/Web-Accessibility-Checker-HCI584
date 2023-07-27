589ca0bdfed8c9cb50a18e482d9d12cbb1b1b465
Jordan Wood <104526584+thejordanwood@users.noreply.github.com>
2023-07-26 19:21:38 -0500

    Cleaning up final file
    Added better developer documentation

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ from bs4 import BeautifulSoup
 # Creates the Flask application instance
 app = Flask(__name__)
     
-# Defines the function to get alt text image info. 
-# Function puts the web address, thumbnail, presence of alt text, alt text content, and pass/fail score into a table.
+# Defines the function to get alt text image info and put it into a table.  
 def get_image_info(url):
+    """Puts the web address, thumbnail, alt text content, and pass/fail score into a table.
+
+    Args:
+        url (str): The URL of the webpage to fetch image information from.
+
+    Returns:
+        list: A list of dictionaries, each containing image information (web address, thumbnail,
+              alt text content, and pass/fail score).
+    """
     try:
         response = requests.get(url)
     except requests.exceptions.ConnectionError as e:
@@ -17,8 +25,8 @@ def get_image_info(url):
     # Empty list that stores image info dicts
     image_info = []
     
-    # Processes the image's web address and thumbnail, and calculates pass/fail score of alt text
     def process_img_tag(img):
+        """Processes the image's web address and thumbnail, and calculates pass/fail score of alt text"""
         image_data = {}
         image_data['Web address'] = url
         if 'src' in img.attrs:
@@ -52,8 +60,16 @@ def get_image_info(url):
     
     return image_info
 
-# Function to calculate pass/fail score for alt text based on word count and specific words
+
 def calculate_score_img(alt_text):
+    """Calculates pass/fail score for alt text based on word count and specific words.
+
+    Args:
+        alt_text (str): The alt text of an image.
+
+    Returns:
+        str: Returns "Pass" if alt text meets the criteria, otherwise "Fail".
+    """
     word_count = len(alt_text.split())
     # Checks if there are any empty alt text tags. This could mean the image is decorative, which means it doesn't need alt text.
     if alt_text == " ":
@@ -65,9 +81,17 @@ def calculate_score_img(alt_text):
             return "Pass"  
     return "Fail"
 
-# Defines function to get link text image info
-# Function puts the destination address, link text content, and pass/fail score into a table. 
+# Defines function to get link text image info 
 def get_link_text_info(url):
+    """Puts the destination address, link text content, and pass/fail score into a table.
+
+    Args:
+        url (str): The URL of the webpage to fetch link text information from.
+
+    Returns:
+        list: A list of dictionaries, each containing link text information (link destination,
+              link text content, and pass/fail score).
+    """
     try:
         response = requests.get(url)
     except requests.exceptions.ConnectionError as e:
@@ -77,8 +101,8 @@ def get_link_text_info(url):
     # Empty list that stores link text info dicts
     link_text_info = []
     
-    # Processes the link's destination address and link text content
     def process_a_tag(a):
+        """Processes the link's destination address and link text content"""
         link_data = {}
         link_data['Web address'] = url
         link_data['Link text content'] = a.get_text()
@@ -112,8 +136,15 @@ def get_link_text_info(url):
     
     return link_text_info
 
-# Function to calculate pass/fail score of link text based on word count and specific words
 def calculate_score_link(link_text):
+    """Calculates pass/fail score of link text based on word count and specific words.
+
+    Args:
+        link_text (str): The link text.
+
+    Returns:
+        str: Returns "Pass" if link text meets the criteria, otherwise "Fail".
+    """
     word_count = len(link_text.split())
     # Checks if link text is between 4 - 30 words
     if 4 <= word_count <= 30: 
@@ -122,9 +153,13 @@ def calculate_score_link(link_text):
             return "Pass"       
     return "Fail"
 
-# Starts screen where user can insert web address and select which table to view
 @app.route('/', methods=['GET', 'POST'])
 def start_screen():
+    """Starts screen where the user can insert a web address and select which table to view.
+
+    Returns:
+        str: HTML template with the appropriate table data and website URL.
+    """
     try:
         website_url = request.args.get('website_url', default='')
         table_type = request.args.get('table_type', default='None')
@@ -150,9 +185,13 @@ def start_screen():
     else:
         return render_template('template.html', table_type='None', website_url=website_url)
 
-# This checks and receives the table data that will be exported into a csv
 @app.route('/export', methods=['GET'])
 def export_table():
+    """Checks and receives the table data that will be exported into a CSV.
+
+    Returns:
+        flask.Response: The CSV file as a response object for download.
+    """
     website_url = request.args.get('website_url')
     table_type = request.args.get('table_type')
 
@@ -175,8 +214,15 @@ def export_table():
     
     return response
 
-# This generates the csv that gets downloaded
 def generate_csv(table_data):
+    """Generates the CSV that gets downloaded.
+
+    Args:
+        table_data (list): A list of dictionaries containing table data.
+
+    Returns:
+        str: The CSV content as a string.
+    """
     if not table_data:
         return ''
 
